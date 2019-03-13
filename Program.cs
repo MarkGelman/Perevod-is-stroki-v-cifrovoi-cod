ed4734e88a67795292ca616937b277270b31ceae
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-13 20:51:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iz_bukv_v_cifrovoi_kod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Вводится 5 символов - слово.
+            Вывести в строчку коды букв этого слова.*/
+
+            string s = Console.ReadLine();
+            Console.WriteLine("{0} {1} {2} {3} {4}", (byte)s[0], (byte)s[1], (byte)s[2], (byte)s[3], (byte)s[4]);
+        }
+    }
+}
